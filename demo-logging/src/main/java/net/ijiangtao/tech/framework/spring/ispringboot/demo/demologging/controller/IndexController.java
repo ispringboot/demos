b7fc347c1ef542f6567a65d8c642c65775fd395a
jiangtao.li <jiangtao.li@baozun.com>
2019-02-04 17:14:50 +0800

    add logging

--- demo-logging/src/main/java/net/ijiangtao/tech/framework/spring/ispringboot/demo/demologging/controller/IndexController.java
+++ demo-logging/src/main/java/net/ijiangtao/tech/framework/spring/ispringboot/demo/demologging/controller/IndexController.java
@@ -2,6 +2,7 @@ package net.ijiangtao.tech.framework.spring.ispringboot.demo.demologging.control
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     private static final Logger LOGGER= LoggerFactory.getLogger(IndexController.class);
+
+    @Value("${demo.log.db.username}")
+    private String dbUserName;
+
+//    @Value("${info.author.name}")
+//    private String authorName;
 
     @GetMapping("/log")
     public String log(){
@@ -18,6 +25,9 @@ public class IndexController {
         LOGGER.info("{} info",preFix);
         LOGGER.warn("{} warn",preFix);
         LOGGER.error("{} error",preFix);
+
+        LOGGER.info("******************************** dbUserName={}",dbUserName);
+       // LOGGER.info("******************************** authorName={}",authorName);
 
         return "log has been writtten";
     }
