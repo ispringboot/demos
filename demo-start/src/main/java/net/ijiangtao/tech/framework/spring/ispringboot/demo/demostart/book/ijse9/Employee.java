ddf49e32569deb5101442f89e5181889ee9f8abd
jiangtao.li <jiangtao.li@baozun.com>
2019-02-22 15:19:16 +0800

    add

---
+++ demo-start/src/main/java/net/ijiangtao/tech/framework/spring/ispringboot/demo/demostart/book/ijse9/Employee.java
@@ -0,0 +1,12 @@
+package net.ijiangtao.tech.framework.spring.ispringboot.demo.demostart.book.ijse9;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Employee {
+    private String name;
+}
---
+++ demo-start/src/main/java/net/ijiangtao/tech/framework/spring/ispringboot/demo/demostart/book/ijse9/Manager.java
@@ -0,0 +1,8 @@
+package net.ijiangtao.tech.framework.spring.ispringboot.demo.demostart.book.ijse9;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Manager extends Employee {
+
+}
