b486589e36b80f16f0a484b72f65de5f51b13440
ijiangtao <jiangtao.li@baozun.com>
2019-03-29 17:50:49 +0800

    pop-up demo

---
+++ demo-popup/src/main/java/net/ijiangtao/tech/framework/spring/ispringboot/demo/popup/DemoPopupApplication.java
@@ -0,0 +1,20 @@
+package net.ijiangtao.tech.framework.spring.ispringboot.demo.popup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoPopupApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoPopupApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPopupApplication.class, args);
+	}
+
+}
---
+++ demo-popup/src/main/java/net/ijiangtao/tech/framework/spring/ispringboot/demo/popup/controller/HelloController.java
@@ -0,0 +1,19 @@
+package net.ijiangtao.tech.framework.spring.ispringboot.demo.popup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author ijiangtao.net
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String testThymeleaf(ModelMap modelMap){
+        modelMap.addAttribute("msg", "Hello!");
+        return "hello";
+    }
+}
